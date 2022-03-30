375fa27257a2fd9261192763889beb90a5893a77
unixibi <unixibi@fuck.you>
2022-03-30 11:07:09 -0700

    asdf

--- src/shell/sdl/tac_sdl_main.cpp
+++ src/shell/sdl/tac_sdl_main.cpp
@@ -2,6 +2,24 @@
 #include "src/common/tac_error_handling.h"
 #include "src/common/tac_os.h"
 
+#if __has_include("src/common/tac_os.h")
+#include "src/common/tac_os.h"
+#endif
+
+#if __has_include("src/shell/vulkan/tac_renderer_vulkan.h")
+#include "src/shell/vulkan/tac_renderer_vulkan.h"
+#endif
+
+#if __has_include("tac_renderer_directx11.h")
+// #if __has_include("windows/tacwinlib/renderer/tac_renderer_directx11.h")
+// #if __has_include("shell/windows/tacwinlib/renderer/tac_renderer_directx11.h")
+// #if __has_include("src/shell/windows/tacwinlib/renderer/tac_renderer_directx11.h")
+// #include "src/shell/windows/tacwinlib/renderer/tac_renderer_directx11.h"
+// #include "shell/windows/tacwinlib/renderer/tac_renderer_directx11.h"
+#include "tac_renderer_directx11.h"
+// #include "windows/tacwinlib/renderer/tac_renderer_directx11.h"
+#endif
+
 // Exists a better way?
 #if _WIN32
 #include "src/shell/windows/tacwinlib/renderer/tac_renderer_directx11.h"
@@ -13,6 +31,10 @@ void mainAux( Errors& errors )
 {
   SDLOSInit( errors );
   TAC_HANDLE_ERROR( errors );
+
+#if __has_include("src/shell/windows/tacwinlib/renderer/tac_renderer_directx11.h")
+  Render::RegisterRendererDirectX11();
+#endif
 
 #if _WIN32
   Render::RegisterRendererDirectX11();
