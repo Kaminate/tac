d20880d8190ab8b1e51dde47f507a0aa34c10fb6
nate swole <kaminate@gmail.com>
2022-12-23 16:12:46 -0800

    asdf

--- src/shell/tac_register_renderers.cpp
+++ src/shell/tac_register_renderers.cpp
@@ -1,29 +1,48 @@
 #include "tac_register_renderers.h"
 
+#ifdef TAC_USE_RENDERER_VK
+
+#include "tac_renderer_vulkan.h"
+static int asdf;
+#endif
+
+#ifdef TAC_USE_RENDERER_DX11
+#include "tac_renderer_directx11.h"
+static int fdsa;
+#endif
+
 namespace Tac
 {
 
   // function forward declarations
   namespace Render
   {
+#ifdef TAC_USE_RENDERER_VK
 #if __has_include( "tac_renderer_vulkan.h" )
     void RegisterRendererVulkan();
 #endif
+#endif
 
+#ifdef TAC_USE_RENDERER_DX11
 #if __has_include( "tac_renderer_directx11.h" )
     void RegisterRendererDirectX11();
+#endif
 #endif
   }
 
   // function calls
   void RegisterRenderers()
   {
+#ifdef TAC_USE_RENDERER_VK
 #if __has_include( "tac_renderer_vulkan.h" )
     Render::RegisterRendererVulkan();
 #endif
+#endif
 
+#ifdef TAC_USE_RENDERER_DX11
 #if __has_include( "tac_renderer_directx11.h" )
     Render::RegisterRendererDirectX11();
+#endif
 #endif
 
   }
