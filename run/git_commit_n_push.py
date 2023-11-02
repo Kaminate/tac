5062255dfcac7a1277f84088a4cc129d7828b814
白★能盛 <kaminate@gmail.com>
2023-11-02 12:08:31 -0400

    git_commit_n_push.py

--- run/git_commit_n_push.py
+++ run/git_commit_n_push.py
@@ -11,121 +11,16 @@ import git
 tac_utils.BeginFile(__file__)
 
 print("hi")
-repo = git.Repo(tac_utils.tac_root)
+root_repo = git.Repo(tac_utils.tac_root)
 
-# untracked_files = repo.untracked_files
-# modified_files = repo.index.diff(None)
+change_lines = []
+repos_to_commit = []
 
-# num_untracked = len(untracked_files)
-# num_modified = len(modified_files)
-
-# for d in diffs:
-#     print(d.a_path)
-
-commit_msg = ""
-
-# def GetCommitMsg():
-#   print("Please enter commit msg: ")
-#   commit_msg = input()
-
-def VisitRepo( repo ):
-    global commit_msg
-
-    # untracked_files = repo.untracked_files
-    # diffs = repo.index.diff(None)
-
-    # num_untracked = len(untracked_files)
-    # num_modified = len(modified_files)
-
-    # for file in untracked_files:
-        # print(d.a_path)
-
-    # type is https://gitpython.readthedocs.io/en/stable/reference.html#git.diff.Diff
-    # for diff in diffs:
-        # print(diff.a_path)
-
-    # repo_working_dir_fullpath = repo.working_dir
-    # repo_remote_url = repo.remotes[0].config_reader.get("url")
-
-    # repo.git.add(update=True)
-    # repo.index.add('*')
-
-    # repo.git.add(update=True)
-
-    # if repo.is_dirty(untracked_files=True):
-
-
-
-    if repo.is_dirty():
-        print('repo is dirty')
-
-        modified_files = [item.a_path for item in repo.index.diff(None)]
-        print(modified_files)
-
-        if modified_files:
-            # Add only modified files to the staging area
-            repo.index.add(modified_files)
-
-            # print('modified files begin')
-            # for file in modified_files:
-            #     print( file )
-            # print('modified files end')
-
-    if repo.untracked_files:
-        print('repo has untracked files')
-
-        repo.index.add(repo.untracked_files)
-
-        # print('untracked files begin')
-        # for file in repo.untracked_files:
-        #     print( file )
-        # print('untracked files end')
-
-    # staged_files = [entry.path for entry in repo.index.entries]
-    # staged_files = [blob.path for blob in repo.index.iter_blobs()]
-    staged_files = repo.index.diff("HEAD")
-    if staged_files:
-        print( "staged files begin")
-        for file in staged_files:
-            print( file.a_path)
-        # print( "staged files:", staged_files)
-        print( "staged files end")
-
-
-    index_diff = repo.index.diff(None)
-
-    if index_diff:
-        # There are changes in the index to commit
-        print("Changes in the index to commit:")
-        
-        for change in index_diff:
-            strType = f"Change Type: {change.change_type}"
-            strStatus = f"Change Status: {change.change_type_flags}"
-            strPath = f"File Path: {change.a_path}"
-            print(strtype, strStatus, strPath)
-
-
-        print("Please enter commit msg: ")
-        commit_msg = input()
-
-
-        # diffs = repo.index.diff(None)
-        # if len(diffs) == 0:
-        #   print('no diffs')
-        # else:
-        #   print('begin diffs')
-        #   for diff in diffs :
-        #       print( diff.a_path )
-        #   print('end diff')
-
-
-        # repo.index.commit(commit_msg)
-        # remote = repo.remote(name='origin')
-        # remote.push()
+def PrintRepoPath( repo ):
+    print( f'{repo.working_tree_dir} {repo.remotes.origin.url}' )
 
 ###
 
-# repo.index.commit(commit_msg)
 
 #          | name of remote repositiory you are connected to
 #          v 
@@ -133,30 +28,69 @@ def VisitRepo( repo ):
 
 # repo.submodules
 #   https://gitpython.readthedocs.io/en/stable/reference.html#git.repo.base.Repo.submodules
-for sm in repo.submodules:
-
-    # so sm is probably
-    #     https://gitpython.readthedocs.io/en/stable/reference.html#module-git.objects.submodule.base
 
 
-    if not sm.exists():
-        continue
 
-    if not sm.module_exists():
-        continue
+def VisitRepo(repo):
 
-    print( sm.name )
-    m = sm.module() # type Repo
+    global change_lines
+    global repos_to_commit
 
-    VisitRepo( m );
-    break # temp, only test for 1st sm
+    # Stage all changes
+    repo.git.add(all=True) 
 
-    # sm.branch_name
-    # if sm.module_exists():
-    #   m = sm.module() # of type Repo
-    
-    # assert sm.module_exists()  # the module is available, which doesn't have to be the case.
-    # assert sm.module().working_tree_dir.endswith("gitdb")
-    
+    diffs = repo.head.commit.diff()
+    if diffs:
+
+        # Add this repo as one of the repos to commit to
+        repos_to_commit.append( repo )
+
+
+        # Append to list of changes
+        for diff in diffs:
+            strs = [f'{diff.change_type} {diff.a_path}']
+            if diff.a_path != diff.b_path:
+                strs.extend (['-->', diff.b_path ])
+            change_lines.append(' '.join(strs))
+
+
+
+# ONLY_DO_FIRST_SUBMODULE = True
+
+for sm in root_repo.submodules:
+
+    if sm.exists() and sm.module_exists():
+        repo = sm.module()
+        VisitRepo(repo)
+
+    # if ONLY_DO_FIRST_SUBMODULE:
+        # break
+
+# if not ONLY_DO_FIRST_SUBMODULE:
+VisitRepo(root_repo)
+
+
+if len( repos_to_commit ) != 0:
+
+    # Print changed lines so the user knows what commit msg to write
+    for line in change_lines:
+        print(line)
+
+    print("Please enter commit msg: ")
+    commit_msg = input()
+
+    for repo in repos_to_commit:
+        print( f'pushing to {repo.remotes.origin.url}')
+        try:
+            # Same as git commit -m commit_msg
+            repo.index.commit(commit_msg) 
+
+            origin = repo.remote(name='origin')
+
+            # Same as git push
+            origin.push() 
+
+        except Exception as e:
+            print(e) 
 
 tac_utils.EndFile(__file__)
