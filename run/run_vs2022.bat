40a7d1f5c8ade50834f7777308e3191b9670c5ac
nate swole <kaminate@gmail.com>
2023-12-15 19:50:22 -0500

    some cmake stuff

--- run/run_vs2022.bat
+++ run/run_vs2022.bat
@@ -4,26 +4,13 @@ cls
 :: so the pushd and popd calls later refer to the root tac dir
 pushd "%~dp0"
 
-:: -------------------------------------------------------------------------------------------------
-:: Running from Developer Command Prompt for VS2022 sets $ENV{WindowsSdkVerBinPath} for cmake
-:: -------------------------------------------------------------------------------------------------
-IF NOT defined WindowsSdkVerBinPath (
-  echo "please run this script from Developer Command Prompt for VS2022"
-  pause
-  exit /b 1
-)
-
 :: loop label for goto
 :loop
 
-
 :: change directory from tac/run to tac/ before running cmake
 pushd ..
-cmake -G "Visual Studio 17 2022" -A x64 -B build_vs2022
-
-:: uncomment to see message( VERBOSE "...")
-::
-:: cmake --log-level=VERBOSE -G "Visual Studio 17 2022" -A x64 -B build_vs2022
+:: cmake                   -G "Visual Studio 17 2022" -A x64 -B build_vs2022
+cmake --log-level=VERBOSE -G "Visual Studio 17 2022" -A x64 -B build_vs2022
 
 popd
 
