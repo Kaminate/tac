ab127e26af9335a48fa30ff0ba022699ae52dbf9
nate swole <kaminate@gmail.com>
2024-03-06 20:53:42 -0500

    create tac_win32_file_dialog

--- src/shell/windows/tac_win32_os.cpp
+++ src/shell/windows/tac_win32_os.cpp
@@ -16,6 +16,7 @@
 #include "src/common/system/tac_os.h"
 #include "src/shell/tac_desktop_app.h"
 #include "src/shell/windows/tac_win32.h"
+#include "src/shell/windows/tac_win32_file_dialog.h"
 
 import std; // iostream, filesystem, ctime (mktime )
 
@@ -36,82 +37,6 @@ namespace Tac
       .mHeight = GetSystemMetrics( SM_CYSCREEN ),
     };
   }
-
-  struct FileDialogHelper
-  {
-    enum Type
-    {
-      kOpen,
-      kSave,
-    };
-
-    FileDialogHelper( Type type ) : mType( type ) { }
-
-    ~FileDialogHelper()
-    {
-      CoUninitialize();
-    }
-
-    Filesystem::Path Run( Errors& errors )
-    {
-      TAC_HR_CALL_RET( {}, CoInitializeEx( NULL, COINIT_APARTMENTTHREADED ) );
-      TAC_CALL_RET( {}, CreateDialogInstance( errors ) );
-      TAC_CALL_RET( {}, SetDefaultFolder( errors ) );
-      TAC_CALL_RET( {}, Show( errors ) );
-      if( mCancelled )
-        return {};
-      return GetResult( errors );
-    }
-
-  private:
-    void SetDefaultFolder(Errors& errors)
-    {
-      const Filesystem::Path dir = sShellInitialWorkingDir / AssetPathRootFolderName;
-      const std::wstring wDir = dir.Get().wstring();
-
-      PCom<IShellItem> shDir;
-      TAC_HR_CALL( SHCreateItemFromParsingName(
-                   wDir.c_str(),
-                   NULL,
-                   shDir.iid(),
-                   shDir.ppv() ) );
-      TAC_HR_CALL( mDialog->SetDefaultFolder( ( IShellItem* )shDir ) );
-    }
-
-    void CreateDialogInstance(Errors& errors)
-    {
-      REFCLSID sid = mType == Type::kOpen ? CLSID_FileOpenDialog : CLSID_FileSaveDialog;
-      REFIID iid = mType == Type::kOpen ? mOpenDialog.iid() : mSaveDialog.iid();
-      void** ppv = mType == Type::kOpen ? mOpenDialog.ppv() : mSaveDialog.ppv();
-      TAC_HR_CALL( CoCreateInstance( sid, NULL, CLSCTX_INPROC_SERVER, iid, ppv ) );
-      mDialog = mType == Type::kOpen ? (IFileDialog*)mOpenDialog : (IFileDialog*)mSaveDialog;
-    };
-
-    void Show(Errors& errors)
-    {
-      const HRESULT hr = mDialog->Show( nullptr );
-      mCancelled = hr == HRESULT_FROM_WIN32( ERROR_CANCELLED );
-      TAC_RAISE_ERROR_IF( FAILED( hr ) && !mCancelled, "Failed to show dialog");
-    }
-
-    Filesystem::Path GetResult( Errors& errors )
-    {
-      PCom<IShellItem> pItem;
-      TAC_HR_CALL_RET( {}, mDialog->GetResult( pItem.CreateAddress() ) );
-
-      PWSTR pszFilePath;
-      TAC_HR_CALL_RET( {}, pItem->GetDisplayName( SIGDN_FILESYSPATH, &pszFilePath ) );
-      TAC_ON_DESTRUCT( CoTaskMemFree( pszFilePath ) );
-
-      return std::filesystem::path( pszFilePath );
-    }
-
-    IFileDialog* mDialog = nullptr;
-    PCom<IFileOpenDialog> mOpenDialog;
-    PCom<IFileSaveDialog> mSaveDialog;
-    bool mCancelled = false;
-    Type mType;
-  };
 
   static Filesystem::Path Win32OSOpenDialog( Errors& errors )
   {
